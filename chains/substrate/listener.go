b3cea744f86d1da8c4b749bddb82d4d45e8fc5ce
Karthikeyan <karthikeyan@cere.io>
2021-09-27 21:17:50 +0530

    commented unused functions

--- chains/substrate/listener.go
+++ chains/substrate/listener.go
@@ -6,16 +6,13 @@ package substrate
 import (
 	"errors"
 	"fmt"
-	"math/big"
 	"time"
 
 	"github.com/Cerebellum-Network/ChainBridge/chains"
-	utils "github.com/Cerebellum-Network/ChainBridge/shared/substrate"
 	"github.com/Cerebellum-Network/chainbridge-utils/blockstore"
 	metrics "github.com/Cerebellum-Network/chainbridge-utils/metrics/types"
 	"github.com/Cerebellum-Network/chainbridge-utils/msg"
 	"github.com/ChainSafe/log15"
-	"github.com/centrifuge/go-substrate-rpc-client/v2/types"
 )
 
 type listener struct {
@@ -99,153 +96,153 @@ var ErrBlockNotReady = errors.New("required result to be 32 bytes, but got 0")
 // pollBlocks will poll for the latest block and proceed to parse the associated events as it sees new blocks.
 // Polling begins at the block defined in `l.startBlock`. Failed attempts to fetch the latest block or parse
 // a block will be retried up to BlockRetryLimit times before returning with an error.
-func (l *listener) pollBlocks() error {
-	var currentBlock = l.startBlock
-	var retry = BlockRetryLimit
-	for {
-		select {
-		case <-l.stop:
-			return errors.New("terminated")
-		default:
-			// No more retries, goto next block
-			if retry == 0 {
-				l.sysErr <- fmt.Errorf("event polling retries exceeded (chain=%d, name=%s)", l.chainId, l.name)
-				return nil
-			}
+// func (l *listener) pollBlocks() error {
+// 	var currentBlock = l.startBlock
+// 	var retry = BlockRetryLimit
+// 	for {
+// 		select {
+// 		case <-l.stop:
+// 			return errors.New("terminated")
+// 		default:
+// 			// No more retries, goto next block
+// 			if retry == 0 {
+// 				l.sysErr <- fmt.Errorf("event polling retries exceeded (chain=%d, name=%s)", l.chainId, l.name)
+// 				return nil
+// 			}
 
-			// Get finalized block hash
-			finalizedHash, err := l.conn.api.RPC.Chain.GetFinalizedHead()
-			if err != nil {
-				l.log.Error("Failed to fetch finalized hash", "err", err)
-				retry--
-				time.Sleep(BlockRetryInterval)
-				continue
-			}
+// 			// Get finalized block hash
+// 			finalizedHash, err := l.conn.api.RPC.Chain.GetFinalizedHead()
+// 			if err != nil {
+// 				l.log.Error("Failed to fetch finalized hash", "err", err)
+// 				retry--
+// 				time.Sleep(BlockRetryInterval)
+// 				continue
+// 			}
 
-			// Get finalized block header
-			finalizedHeader, err := l.conn.api.RPC.Chain.GetHeader(finalizedHash)
-			if err != nil {
-				l.log.Error("Failed to fetch finalized header", "err", err)
-				retry--
-				time.Sleep(BlockRetryInterval)
-				continue
-			}
+// 			// Get finalized block header
+// 			finalizedHeader, err := l.conn.api.RPC.Chain.GetHeader(finalizedHash)
+// 			if err != nil {
+// 				l.log.Error("Failed to fetch finalized header", "err", err)
+// 				retry--
+// 				time.Sleep(BlockRetryInterval)
+// 				continue
+// 			}
 
-			if l.metrics != nil {
-				l.metrics.LatestKnownBlock.Set(float64(finalizedHeader.Number))
-			}
+// 			if l.metrics != nil {
+// 				l.metrics.LatestKnownBlock.Set(float64(finalizedHeader.Number))
+// 			}
 
-			// Sleep if the block we want comes after the most recently finalized block
-			if currentBlock > uint64(finalizedHeader.Number) {
-				l.log.Trace("Block not yet finalized", "target", currentBlock, "latest", finalizedHeader.Number)
-				time.Sleep(BlockRetryInterval)
-				continue
-			}
+// 			// Sleep if the block we want comes after the most recently finalized block
+// 			if currentBlock > uint64(finalizedHeader.Number) {
+// 				l.log.Trace("Block not yet finalized", "target", currentBlock, "latest", finalizedHeader.Number)
+// 				time.Sleep(BlockRetryInterval)
+// 				continue
+// 			}
 
-			// Get hash for latest block, sleep and retry if not ready
-			hash, err := l.conn.api.RPC.Chain.GetBlockHash(currentBlock)
-			if err != nil && err.Error() == ErrBlockNotReady.Error() {
-				time.Sleep(BlockRetryInterval)
-				continue
-			} else if err != nil {
-				l.log.Error("Failed to query latest block", "block", currentBlock, "err", err)
-				retry--
-				time.Sleep(BlockRetryInterval)
-				continue
-			}
+// 			// Get hash for latest block, sleep and retry if not ready
+// 			hash, err := l.conn.api.RPC.Chain.GetBlockHash(currentBlock)
+// 			if err != nil && err.Error() == ErrBlockNotReady.Error() {
+// 				time.Sleep(BlockRetryInterval)
+// 				continue
+// 			} else if err != nil {
+// 				l.log.Error("Failed to query latest block", "block", currentBlock, "err", err)
+// 				retry--
+// 				time.Sleep(BlockRetryInterval)
+// 				continue
+// 			}
 
-			err = l.processEvents(hash)
-			if err != nil {
-				l.log.Error("Failed to process events in block", "block", currentBlock, "err", err)
-				retry--
-				continue
-			}
+// 			err = l.processEvents(hash)
+// 			if err != nil {
+// 				l.log.Error("Failed to process events in block", "block", currentBlock, "err", err)
+// 				retry--
+// 				continue
+// 			}
 
-			// Write to blockstore
-			err = l.blockstore.StoreBlock(big.NewInt(0).SetUint64(currentBlock))
-			if err != nil {
-				l.log.Error("Failed to write to blockstore", "err", err)
-			}
+// 			// Write to blockstore
+// 			err = l.blockstore.StoreBlock(big.NewInt(0).SetUint64(currentBlock))
+// 			if err != nil {
+// 				l.log.Error("Failed to write to blockstore", "err", err)
+// 			}
 
-			if l.metrics != nil {
-				l.metrics.BlocksProcessed.Inc()
-				l.metrics.LatestProcessedBlock.Set(float64(currentBlock))
-			}
+// 			if l.metrics != nil {
+// 				l.metrics.BlocksProcessed.Inc()
+// 				l.metrics.LatestProcessedBlock.Set(float64(currentBlock))
+// 			}
 
-			currentBlock++
-			l.latestBlock.Height = big.NewInt(0).SetUint64(currentBlock)
-			l.latestBlock.LastUpdated = time.Now()
-			retry = BlockRetryLimit
-		}
-	}
-}
+// 			currentBlock++
+// 			l.latestBlock.Height = big.NewInt(0).SetUint64(currentBlock)
+// 			l.latestBlock.LastUpdated = time.Now()
+// 			retry = BlockRetryLimit
+// 		}
+// 	}
+// }
 
 // processEvents fetches a block and parses out the events, calling Listener.handleEvents()
-func (l *listener) processEvents(hash types.Hash) error {
-	l.log.Trace("Fetching block for events", "hash", hash.Hex())
-	meta := l.conn.getMetadata()
-	key, err := types.CreateStorageKey(&meta, "System", "Events", nil, nil)
-	if err != nil {
-		return err
-	}
+// func (l *listener) processEvents(hash types.Hash) error {
+// 	l.log.Trace("Fetching block for events", "hash", hash.Hex())
+// 	meta := l.conn.getMetadata()
+// 	key, err := types.CreateStorageKey(&meta, "System", "Events", nil, nil)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	var records types.EventRecordsRaw
-	_, err = l.conn.api.RPC.State.GetStorage(key, &records, hash)
-	if err != nil {
-		return err
-	}
+// 	var records types.EventRecordsRaw
+// 	_, err = l.conn.api.RPC.State.GetStorage(key, &records, hash)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	e := utils.Events{}
-	err = records.DecodeEventRecords(&meta, &e)
-	if err != nil {
-		return err
-	}
+// 	e := utils.Events{}
+// 	err = records.DecodeEventRecords(&meta, &e)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	l.handleEvents(e)
-	l.log.Trace("Finished processing events", "block", hash.Hex())
+// 	l.handleEvents(e)
+// 	l.log.Trace("Finished processing events", "block", hash.Hex())
 
-	return nil
-}
+// 	return nil
+// }
 
 // handleEvents calls the associated handler for all registered event types
-func (l *listener) handleEvents(evts utils.Events) {
-	if l.subscriptions[FungibleTransfer] != nil {
-		for _, evt := range evts.ChainBridge_FungibleTransfer {
-			l.log.Trace("Handling FungibleTransfer event")
-			l.submitMessage(l.subscriptions[FungibleTransfer](evt, l.log))
-		}
-	}
-	if l.subscriptions[NonFungibleTransfer] != nil {
-		for _, evt := range evts.ChainBridge_NonFungibleTransfer {
-			l.log.Trace("Handling NonFungibleTransfer event")
-			l.submitMessage(l.subscriptions[NonFungibleTransfer](evt, l.log))
-		}
-	}
-	if l.subscriptions[GenericTransfer] != nil {
-		for _, evt := range evts.ChainBridge_GenericTransfer {
-			l.log.Trace("Handling GenericTransfer event")
-			l.submitMessage(l.subscriptions[GenericTransfer](evt, l.log))
-		}
-	}
+// func (l *listener) handleEvents(evts utils.Events) {
+// 	if l.subscriptions[FungibleTransfer] != nil {
+// 		for _, evt := range evts.ChainBridge_FungibleTransfer {
+// 			l.log.Trace("Handling FungibleTransfer event")
+// 			l.submitMessage(l.subscriptions[FungibleTransfer](evt, l.log))
+// 		}
+// 	}
+// 	if l.subscriptions[NonFungibleTransfer] != nil {
+// 		for _, evt := range evts.ChainBridge_NonFungibleTransfer {
+// 			l.log.Trace("Handling NonFungibleTransfer event")
+// 			l.submitMessage(l.subscriptions[NonFungibleTransfer](evt, l.log))
+// 		}
+// 	}
+// 	if l.subscriptions[GenericTransfer] != nil {
+// 		for _, evt := range evts.ChainBridge_GenericTransfer {
+// 			l.log.Trace("Handling GenericTransfer event")
+// 			l.submitMessage(l.subscriptions[GenericTransfer](evt, l.log))
+// 		}
+// 	}
 
-	if len(evts.System_CodeUpdated) > 0 {
-		l.log.Trace("Received CodeUpdated event")
-		err := l.conn.updateMetatdata()
-		if err != nil {
-			l.log.Error("Unable to update Metadata", "error", err)
-		}
-	}
-}
+// 	if len(evts.System_CodeUpdated) > 0 {
+// 		l.log.Trace("Received CodeUpdated event")
+// 		err := l.conn.updateMetatdata()
+// 		if err != nil {
+// 			l.log.Error("Unable to update Metadata", "error", err)
+// 		}
+// 	}
+// }
 
 // submitMessage inserts the chainId into the msg and sends it to the router
-func (l *listener) submitMessage(m msg.Message, err error) {
-	if err != nil {
-		log15.Error("Critical error processing event", "err", err)
-		return
-	}
-	m.Source = l.chainId
-	err = l.router.Send(m)
-	if err != nil {
-		log15.Error("failed to process event", "err", err)
-	}
-}
+// func (l *listener) submitMessage(m msg.Message, err error) {
+// 	if err != nil {
+// 		log15.Error("Critical error processing event", "err", err)
+// 		return
+// 	}
+// 	m.Source = l.chainId
+// 	err = l.router.Send(m)
+// 	if err != nil {
+// 		log15.Error("failed to process event", "err", err)
+// 	}
+// }
