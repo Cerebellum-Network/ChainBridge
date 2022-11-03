9fd0f34213a14ebac788b395fef3252b0f119f1e
Dmitriy Molchanov <dmitiry.molchanov@iamlanistar.com>
2022-11-03 16:49:21 +0300

    Fix nonFungibleProposal test

--- shared/substrate/query.go
+++ shared/substrate/query.go
@@ -22,7 +22,7 @@ func QueryStorage(client *Client, prefix, method string, arg1, arg2 []byte, resu
 // TODO: Add to GSRPC
 func getConst(meta *types.Metadata, prefix, name string, res interface{}) error {
 	consValue, err := meta.AsMetadataV14.FindConstantValue(types.NewText(prefix), types.NewText(name))
-	codec.Decode(consValue, consValue)
+	codec.Decode(consValue, res)
 	return err
 }
 
